5ba0701025d4d3c1fcf4b4d99d1503aac9cf810e
Fergus Cooper <fcooper8472@gmail.com>
2021-08-05 17:57:24 +0100

    Update regression test to use new API

--- test/test_regression.cpp
+++ test/test_regression.cpp
@@ -16,53 +16,85 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
-#include "HMM.hpp"
+#include "ASMC.hpp"
 #include "FileUtils.hpp"
+
+#include <Eigen/Core>
+#include <fmt/format.h>
+
 #include <sstream>
 
-TEST_CASE("test hmm with regression test", "[HMM_regression]")
+TEST_CASE("test ASMC regression", "[HMM_regression]")
 {
   // we only needed to set doPosteriorSums to true, but because C++ does
   // not have keyword arguments we need to go through everything
-  DecodingParams params(
-      ASMC_FILE_DIR "/EXAMPLE/exampleFile.n300.array",
-      ASMC_FILE_DIR "/DECODING_QUANTITIES/30-100-2000.decodingQuantities.gz",
-      "", // _outFileRoot
-      1, // _jobs
-      1, // _jobInd
-      "array", // _decodingModeString
-      false, // _decodingSequence
-      true, // _usingCSFS
-      false, // _compress
-      false, // _useAncestral
-      0.f, // _skipCSFSdistance
-      false, // _noBatches
-      true // _doPosteriorSums
-      );
+  DecodingParams params(ASMC_DATA_DIR "/examples/asmc/exampleFile.n300.array",
+                        ASMC_DATA_DIR "/decoding_quantities/30-100-2000_CEU.decodingQuantities.gz",
+                        "",      // _outFileRoot
+                        1,       // _jobs
+                        1,       // _jobInd
+                        "array", // _decodingModeString
+                        false,   // _decodingSequence
+                        true,    // _usingCSFS
+                        false,   // _compress
+                        false,   // _useAncestral
+                        0.f,     // _skipCSFSdistance
+                        false,   // _noBatches
+                        true     // _doPosteriorSums
+  );
 
-  Data data(params);
-  HMM hmm(data, params);
+  std::vector<unsigned long> indToDecodeA = {1ul, 2ul, 3ul};
+  std::vector<unsigned long> indToDecodeB = {2ul, 3ul, 4ul};
 
-  REQUIRE(data.individuals.size() > 20);
+  ASMC::ASMC asmc(params);
+  asmc.setStorePerPairPosteriorMean();
+  asmc.setStorePerPairMap();
+  asmc.decodePairs(indToDecodeA, indToDecodeB);
 
-  SECTION("regression test")
+  auto res = asmc.getRefOfResults();
+
+  SECTION("regression test per pair posterior means")
   {
-    std::string regressionFile = ASMC_FILE_DIR "/../ASMC_SRC/TESTS/data/regression_test_original.gz";
+
+    CHECK(res.perPairPosteriorMeans.rows() == 3ll);
+    CHECK(res.perPairPosteriorMeans.cols() == 6760ll);
+
+    std::string regressionFile = ASMC_DATA_DIR "/testing/asmc/regression/regression.perPairPosteriorMeans.gz";
     FileUtils::AutoGzIfstream fin;
     fin.openOrExit(regressionFile);
-    hmm.decodeAll(params.jobs, params.jobInd);
-    const DecodingReturnValues& decodingReturnValues = hmm.getDecodingReturnValues();
-    int pos = 0;
-    for( std::string line; getline(fin, line); )
-    {
-      std::ostringstream oss;
-      for (uint k = 0; k < hmm.getDecodingQuantities().states; k++) {
-        if (k) oss << "\t";
-        oss << decodingReturnValues.sumOverPairs(pos,k);
+
+    for (auto rowIdx = 0ul; rowIdx < indToDecodeA.size(); ++rowIdx) {
+      std::string line;
+      getline(fin, line);
+      std::istringstream iss(line);
+      std::vector<float> rowAsFloats = {std::istream_iterator<float>(iss), std::istream_iterator<float>()};
+
+      CHECK(rowAsFloats.size() == 6760ul);
+      for (auto colIdx = 0ul; colIdx < rowAsFloats.size(); ++colIdx) {
+        CHECK(res.perPairPosteriorMeans(rowIdx, colIdx) == Approx(rowAsFloats.at(colIdx)));
       }
-      REQUIRE(oss.str() == line);
-      pos++;
     }
-    REQUIRE(pos == decodingReturnValues.sumOverPairs.rows());
+  }
+
+  SECTION("regression test per pair MAP")
+  {
+    CHECK(res.perPairMAPs.rows() == 3ll);
+    CHECK(res.perPairMAPs.cols() == 6760ll);
+
+    std::string regressionFile = ASMC_DATA_DIR "/testing/asmc/regression/regression.perPairMAP.gz";
+    FileUtils::AutoGzIfstream fin;
+    fin.openOrExit(regressionFile);
+
+    for (auto rowIdx = 0ul; rowIdx < indToDecodeA.size(); ++rowIdx) {
+      std::string line;
+      getline(fin, line);
+      std::istringstream iss(line);
+      std::vector<int> rowAsInts = {std::istream_iterator<int>(iss), std::istream_iterator<int>()};
+
+      CHECK(rowAsInts.size() == 6760ul);
+      for (auto colIdx = 0ul; colIdx < rowAsInts.size(); ++colIdx) {
+        CHECK(res.perPairMAPs(rowIdx, colIdx) == rowAsInts.at(colIdx));
+      }
+    }
   }
 }
