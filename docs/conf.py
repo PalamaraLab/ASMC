b2aa0663cbaa675495f19e696b4371c4e50fdb26
Fergus Cooper <fcooper8472@gmail.com>
2023-09-22 12:35:47 +0200

    Update documentation

--- docs/conf.py
+++ docs/conf.py
@@ -18,9 +18,9 @@
 # -- Project information -----------------------------------------------------
 
 project = 'ASMC'
-copyright = '2021, Palamara Lab'
-author = 'Palamara Lab'
-
+copyright = '2023, ASMC Developers'
+author = 'ASMC Developers, https://palamaralab.github.io/software/asmc/'
+release = 'v1.3.1'
 
 # -- General configuration ---------------------------------------------------
 
@@ -28,6 +28,7 @@ author = 'Palamara Lab'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -44,7 +45,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- docs/index.rst
+++ docs/index.rst
@@ -3,8 +3,8 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to ASMC's documentation!
-================================
+Welcome to the ASMC documentation!
+==================================
 
 .. toctree::
    :maxdepth: 2
@@ -22,6 +22,4 @@ Welcome to ASMC's documentation!
 Indices and tables
 ==================
 
-* :ref:`genindex`
-* :ref:`modindex`
 * :ref:`search`
